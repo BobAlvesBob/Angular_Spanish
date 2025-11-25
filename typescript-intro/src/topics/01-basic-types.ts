c673983e9f5681927b7178b9dd128cfa0a36b873
Bob <bobpitaia@gmail.com>
2025-11-25 15:38:52 +0000

    Tipos básicos y conceptos generales

---
+++ typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Strider';
+let hpPoints: number  | 'Empty'= 95;
+const isAlive: boolean = true;
+
+hpPoints = 'Empty';
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+export {};
