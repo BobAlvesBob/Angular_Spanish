87590f0997f5debd7b06a9c651e8c678f1d543a6
Bob <bobpitaia@gmail.com>
2025-11-26 23:45:15 +0000

    homework-types

--- typescript-intro/src/topics/04-homework-types.ts
+++ typescript-intro/src/topics/04-homework-types.ts
@@ -1,15 +1,26 @@
+interface SuperHeroe{
+    name: string;
+    age: number;
+    address: Address;
+    showAddress: () => string;
+}
+interface Address {
+     street: string;
+        country: string;
+        city: string;
+}
+    
 
-
-const superHeroe: SuperHero = {
+const superHeroe: SuperHeroe = {
     name: 'Spiderman',
     age: 30,
     address: {
-        calle: 'Main St',
-        pais: 'USA',
-        ciudad: 'NY'
+        street: 'Main St',
+        country: 'USA',
+        city: 'NY'
     },
     showAddress() {
-        return this.name + ', ' + this.address.ciudad + ', ' + this.address.pais;
+        return this.name + ', ' + this.address.city + ', ' + this.address.country;
     }
 }
 
