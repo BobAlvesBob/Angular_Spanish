007236fc841589725ffe4978c7c6b6285ff4f341
Bob <bobpitaia@gmail.com>
2025-11-30 17:44:48 +0000

    Destructuracion de Arreglos

--- typescript-intro/src/topics/05-basic-destructuring.ts
+++ typescript-intro/src/topics/05-basic-destructuring.ts
@@ -33,11 +33,19 @@ const { song: anotherSong,
  const {author: newAuthor} = details
 
 
-console.log('Song: ', song);
-console.log('Author:', audioPlayer.details.author);
-console.log('Duration:', audioPlayer.songDuration);
-console.log(duration, anotherSong)
-console.log('Author:', newAuthor);
+// console.log('Song: ', song);
+// console.log('Author:', audioPlayer.details.author);
+// console.log('Duration:', audioPlayer.songDuration);
+// console.log(duration, anotherSong)
+// console.log('Author:', newAuthor);
+
+// const dbz: string[] = ['Goku', 'Trunks', 'Vegeta']
+
+// const trunk = dbz[3] || 'No hay personaje'
+// console.error('Personaje 3: ', dbz[3] || 'no hay personaje');
+// console.log(dbz[3]);
+const [, , trunks = 'Not Foundz']: string[] = ['Goku', 'Vegeta'];
+ console.error('Personaje 3: ', trunks);
 
 
 
